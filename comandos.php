9b83803164b912460df9f2354586f3f453152b4a
Victor Beltrão <victorbeltraoduarte@gmail.com>
2025-04-12 12:43:00 -0400

    Refactor/ComandosPHP

--- comandos.php
+++ comandos.php
@@ -3,3 +3,37 @@
     echo"Hello, World!";
 ?>
 
+//Comentarios
+/**
+Uso de blockdoc no PHP
+*/
+
+//Funcoes de saida
+
+
+<?php
+//Quebras de linhas
+    echo "Hello" . "," . "World" . PHP_EOL;
+?>
+
+
+<?php
+/**
+ * Utilizado para verificar quais os tipos de dados e qual a linha que esses dados estao.
+ * Tipos de dados, str, boolean, int, float, double, arrays 
+ */
+
+var_dump("true");
+var_dump("[1,2,3,4]");
+var_dump("12");
+var_dump("12.29");
+?>
+
+<?php
+//variaveis e constantes
+$dados = ['NoNamed', 99, 'Azkaban'];
+var_dump($dados[0]);
+
+
+
+?>
